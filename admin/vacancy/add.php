943a46b7980e4382278eb1812c74fffca6085ddf
Hauzan Ariq Bakri <hauzanbakri@gmail.com>
2024-06-27 15:58:33 +0700

    initial commit

---
+++ admin/vacancy/add.php
@@ -0,0 +1,162 @@
+
+<?php
+     if (!isset($_SESSION['ADMIN_USERID'])){
+      redirect(web_root."admin/index.php");
+     }
+
+?>
+ <form class="form-horizontal span6" action="controller.php?action=add" method="POST">
+
+                <div class="row">
+                   <div class="col-lg-12">
+                      <h1 class="page-header">Tambahkan Lowongan Kerja Baru</h1>
+                    </div>
+                    <!-- /.col-lg-12 -->
+                 </div> 
+
+                 <div class="form-group">
+                    <div class="col-md-8">
+                      <label class="col-md-4 control-label" for=
+                      "COMPANYNAME">Nama Perusahaan:</label>
+
+                      <div class="col-md-8">
+                        <select class="form-control input-sm" id="COMPANYID" name="COMPANYID">
+                          <option value="None">Pilih</option>
+                          <?php 
+                            $sql ="Select * From tblcompany";
+                            $mydb->setQuery($sql);
+                            $res  = $mydb->loadResultList();
+                            foreach ($res as $row) {
+                              # code...
+                              echo '<option value='.$row->COMPANYID.'>'.$row->COMPANYNAME.'</option>';
+                            }
+
+                          ?>
+                        </select>
+                      </div>
+                    </div>
+                  </div>  
+
+                     <div class="form-group">
+                    <div class="col-md-8">
+                      <label class="col-md-4 control-label" for=
+                      "CATEGORY">Katagori :</label>
+
+                      <div class="col-md-8">
+                        <select class="form-control input-sm" id="CATEGORY" name="CATEGORY">
+                          <option value="None">Pilih</option>
+                          <?php 
+                            $sql ="Select * From tblcategory";
+                            $mydb->setQuery($sql);
+                            $res  = $mydb->loadResultList();
+                            foreach ($res as $row) {
+                              # code...
+                              echo '<option value='.$row->CATEGORYID.'>'.$row->CATEGORY.'</option>';
+                            }
+
+                          ?>
+                        </select>
+                      </div>
+                    </div>
+                  </div>  
+                  <div class="form-group">
+                    <div class="col-md-8">
+                      <label class="col-md-4 control-label" for=
+                      "OCCUPATIONTITLE">Judul Pekerjaan:</label> 
+                      <div class="col-md-8">
+                         <input class="form-control input-sm" id="OCCUPATIONTITLE" name="OCCUPATIONTITLE" placeholder="Occupation Title"   autocomplete="none"/> 
+                      </div>
+                    </div>
+                  </div>  
+
+                    <div class="form-group">
+                    <div class="col-md-8">
+                      <label class="col-md-4 control-label" for=
+                      "REQ_NO_EMPLOYEES">Diperlukan tidak. Karyawan:</label> 
+                      <div class="col-md-8">
+                         <input class="form-control input-sm" id="REQ_NO_EMPLOYEES" name="REQ_NO_EMPLOYEES" placeholder="Required no. of Employees"   autocomplete="none"/> 
+                      </div>
+                    </div>
+                  </div>  
+
+                  <div class="form-group">
+                    <div class="col-md-8">
+                      <label class="col-md-4 control-label" for=
+                      "SALARIES">Salary:</label> 
+                      <div class="col-md-8">
+                         <input class="form-control input-sm" id="SALARIES" name="SALARIES" placeholder="Salary"   autocomplete="none"/> 
+                      </div>
+                    </div>
+                  </div>  
+
+                  <div class="form-group">
+                    <div class="col-md-8">
+                      <label class="col-md-4 control-label" for=
+                      "DURATION_EMPLOYEMENT">Durasi Pekerjaan:</label> 
+                      <div class="col-md-8">
+                         <input class="form-control input-sm" id="DURATION_EMPLOYEMENT" name="DURATION_EMPLOYEMENT" placeholder="Duration of Employment"   autocomplete="none"/> 
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="col-md-8">
+                      <label class="col-md-4 control-label" for=
+                      "QUALIFICATION_WORKEXPERIENCE">Kualifikasi/Pengalaman Kerja:</label> 
+                      <div class="col-md-8">
+                        <textarea class="form-control input-sm" id="QUALIFICATION_WORKEXPERIENCE" name="QUALIFICATION_WORKEXPERIENCE" placeholder="Qualification/Work Experience"   autocomplete="none"></textarea> 
+                      </div>
+                    </div>
+                  </div> 
+
+                  <div class="form-group">
+                    <div class="col-md-8">
+                      <label class="col-md-4 control-label" for=
+                      "JOBDESCRIPTION">Uraian Tugas:</label> 
+                      <div class="col-md-8">
+                        <textarea class="form-control input-sm" id="JOBDESCRIPTION" name="JOBDESCRIPTION" placeholder="Job Description"   autocomplete="none"></textarea> 
+                      </div>
+                    </div>
+                  </div>  
+
+                 <div class="form-group">
+                    <div class="col-md-8">
+                      <label class="col-md-4 control-label" for=
+                      "PREFEREDSEX">Jenis kelamin:</label> 
+                      <div class="col-md-8">
+                          <select class="form-control input-sm" id="PREFEREDSEX" name="PREFEREDSEX">
+                          <option value="None">Select</option>
+                           <option>Pria</option>
+                           <option>Wanita</option>
+                           <option>Pria/Wanita
+                      </div>
+                    </div>
+                  </div>  
+
+                  <div class="form-group">
+                    <div class="col-md-8">
+                      <label class="col-md-4 control-label" for=
+                      "SECTOR_VACANCY">Sektor Lowongan:</label> 
+                      <div class="col-md-8">
+                        <textarea class="form-control input-sm" id="SECTOR_VACANCY" name="SECTOR_VACANCY" placeholder="Sector of Vacancy"   autocomplete="none"></textarea> 
+                      </div>
+                    </div>
+                  </div>   
+                  
+                  <div class="form-group">
+                    <div class="col-md-8">
+                      <label class="col-md-4 control-label" for=
+                      "idno"></label>  
+
+                      <div class="col-md-8">
+                         <button class="btn btn-primary btn-sm" name="save" type="submit" ><span class="fa fa-save fw-fa"></span> Save</button>
+                      <!-- <a href="index.php" class="btn btn-info"><span class="glyphicon glyphicon-arrow-left"></span>&nbsp;<strong>Back</strong></a> -->
+                     
+                     </div>
+                    </div>
+                  </div> 
+ 
+          
+        </form>
+      
+ 
---
+++ erisdb.sql
@@ -0,0 +1,420 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 16 Agu 2023 pada 07.31
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `id22126298_erisdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tblapplicants`
+--
+
+CREATE TABLE `tblapplicants` (
+  `APPLICANTID` int(11) NOT NULL,
+  `FNAME` varchar(90) NOT NULL,
+  `LNAME` varchar(90) NOT NULL,
+  `MNAME` varchar(90) NOT NULL,
+  `ADDRESS` varchar(255) NOT NULL,
+  `SEX` varchar(11) NOT NULL,
+  `CIVILSTATUS` varchar(30) NOT NULL,
+  `BIRTHDATE` date NOT NULL,
+  `BIRTHPLACE` varchar(255) NOT NULL,
+  `AGE` int(2) NOT NULL,
+  `USERNAME` varchar(90) NOT NULL,
+  `PASS` varchar(90) NOT NULL,
+  `EMAILADDRESS` varchar(90) NOT NULL,
+  `CONTACTNO` varchar(90) NOT NULL,
+  `DEGREE` text NOT NULL,
+  `APPLICANTPHOTO` varchar(255) NOT NULL,
+  `NATIONALID` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `tblapplicants`
+--
+
+INSERT INTO `tblapplicants` (`APPLICANTID`, `FNAME`, `LNAME`, `MNAME`, `ADDRESS`, `SEX`, `CIVILSTATUS`, `BIRTHDATE`, `BIRTHPLACE`, `AGE`, `USERNAME`, `PASS`, `EMAILADDRESS`, `CONTACTNO`, `DEGREE`, `APPLICANTPHOTO`, `NATIONALID`) VALUES
+(2018013, 'Kim', 'Domingo', 'Enoe', 'Kab City', 'Female', 'none', '1991-01-01', 'Kab Citys', 27, 'kim', 'a6312121e15caec74845b7ba5af23330d52d4ac0', 'kim@y.com', '5415456', 'BSAC', 'photos/RobloxScreenShot20180406_203758793.png', ''),
+(2018014, 'Jake', 'Zyrus', 'Ilmba', 'Kab City', 'Female', 'none', '1993-01-16', 'Kab City', 25, 'jake', 'c8d99c2f7cd5f432c163abcd422672b9f77550bb', 'jake@y.com', '14655623123123', 'BSIT', '', ''),
+(2018015, 'Janry', 'Tan', 'Lim', 'brgy 1 Kab City', 'Female', 'Single', '1992-01-30', 'Kab City', 26, 'janry', '1dd4efc811372cd1efe855981a8863d10ddde1ca', 'jan@gmail.com', '0234234', 'BSIT', '', ''),
+(2023016, 'tes', 'yes', 'sjd', 'JAKARATA', 'Female', 'Single', '1993-10-10', 'JAKARTA', 29, 'tes12@gmail.com', 'b6f3c9436e50229d2b6f2546f6bd30c6e31af73c', 'tes@gmail.com', '0817286272', 'TES', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tblattachmentfile`
+--
+
+CREATE TABLE `tblattachmentfile` (
+  `ID` int(11) NOT NULL,
+  `FILEID` varchar(30) DEFAULT NULL,
+  `JOBID` int(11) NOT NULL,
+  `FILE_NAME` varchar(90) NOT NULL,
+  `FILE_LOCATION` varchar(255) NOT NULL,
+  `USERATTACHMENTID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `tblattachmentfile`
+--
+
+INSERT INTO `tblattachmentfile` (`ID`, `FILEID`, `JOBID`, `FILE_NAME`, `FILE_LOCATION`, `USERATTACHMENTID`) VALUES
+(2, '2147483647', 2, 'Resume', 'photos/27052018124027PLATENO FE95483.docx', 2018013),
+(3, '20236912529', 3, 'Resume', 'photos/16082023065518FREEBIES - SIAGA BENCANA - www.SafetySign.co.pdf', 2023016);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tblautonumbers`
+--
+
+CREATE TABLE `tblautonumbers` (
+  `AUTOID` int(11) NOT NULL,
+  `AUTOSTART` varchar(30) NOT NULL,
+  `AUTOEND` int(11) NOT NULL,
+  `AUTOINC` int(11) NOT NULL,
+  `AUTOKEY` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `tblautonumbers`
+--
+
+INSERT INTO `tblautonumbers` (`AUTOID`, `AUTOSTART`, `AUTOEND`, `AUTOINC`, `AUTOKEY`) VALUES
+(1, '02983', 7, 1, 'userid'),
+(2, '000', 78, 1, 'employeeid'),
+(3, '0', 17, 1, 'APPLICANT'),
+(4, '69125', 30, 1, 'FILEID');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tblcategory`
+--
+
+CREATE TABLE `tblcategory` (
+  `CATEGORYID` int(11) NOT NULL,
+  `CATEGORY` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `tblcategory`
+--
+
+INSERT INTO `tblcategory` (`CATEGORYID`, `CATEGORY`) VALUES
+(10, 'Technology'),
+(11, 'Managerial'),
+(12, 'Engineer'),
+(13, 'IT'),
+(14, 'Civil Engineer'),
+(15, 'HR'),
+(23, 'Sales'),
+(24, 'Banking'),
+(25, 'Finance'),
+(26, 'BPO'),
+(27, 'Degital Marketing'),
+(28, 'Shipping');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tblcompany`
+--
+
+CREATE TABLE `tblcompany` (
+  `COMPANYID` int(11) NOT NULL,
+  `COMPANYNAME` varchar(90) NOT NULL,
+  `COMPANYADDRESS` varchar(90) NOT NULL,
+  `COMPANYCONTACTNO` varchar(30) NOT NULL,
+  `COMPANYSTATUS` varchar(90) NOT NULL,
+  `COMPANYMISSION` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `tblcompany`
+--
+
+INSERT INTO `tblcompany` (`COMPANYID`, `COMPANYNAME`, `COMPANYADDRESS`, `COMPANYCONTACTNO`, `COMPANYSTATUS`, `COMPANYMISSION`) VALUES
+(3, 'Pertamina', 'Jakarta', '081827822', '', ''),
+(4, 'PT.Pertambanagan', 'kalimantan', '08172867282', '', ''),
+(6, 'PT.Demo', 'Bekasi', '086625656899', '', ''),
+(7, 'IT Company', 'Kabankalan City', '04564123', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tblemployees`
+--
+
+CREATE TABLE `tblemployees` (
+  `INCID` int(11) NOT NULL,
+  `EMPLOYEEID` varchar(30) NOT NULL,
+  `FNAME` varchar(50) NOT NULL,
+  `LNAME` varchar(50) NOT NULL,
+  `MNAME` varchar(50) NOT NULL,
+  `ADDRESS` varchar(90) NOT NULL,
+  `BIRTHDATE` date NOT NULL,
+  `BIRTHPLACE` varchar(90) NOT NULL,
+  `AGE` int(11) NOT NULL,
+  `SEX` varchar(30) NOT NULL,
+  `CIVILSTATUS` varchar(30) NOT NULL,
+  `TELNO` varchar(40) NOT NULL,
+  `EMP_EMAILADDRESS` varchar(90) NOT NULL,
+  `CELLNO` varchar(30) NOT NULL,
+  `POSITION` varchar(50) NOT NULL,
+  `WORKSTATS` varchar(90) NOT NULL,
+  `EMPPHOTO` varchar(255) NOT NULL,
+  `EMPUSERNAME` varchar(90) NOT NULL,
+  `EMPPASSWORD` varchar(125) NOT NULL,
+  `DATEHIRED` date NOT NULL,
+  `COMPANYID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `tblemployees`
+--
+
+INSERT INTO `tblemployees` (`INCID`, `EMPLOYEEID`, `FNAME`, `LNAME`, `MNAME`, `ADDRESS`, `BIRTHDATE`, `BIRTHPLACE`, `AGE`, `SEX`, `CIVILSTATUS`, `TELNO`, `EMP_EMAILADDRESS`, `CELLNO`, `POSITION`, `WORKSTATS`, `EMPPHOTO`, `EMPUSERNAME`, `EMPPASSWORD`, `DATEHIRED`, `COMPANYID`) VALUES
+(76, '2018001', 'Chambe', 'Narciso', 'Captain', 'mabinay', '1992-01-23', 'Mabinay', 26, 'Male', 'Married', '032656', 'chambe@yahoo.com', '', 'Fuel Tender', '', '', '2018001', 'f3593fd40c55c33d1788309d4137e82f5eab0dea', '2018-05-23', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tblfeedback`
+--
+
+CREATE TABLE `tblfeedback` (
+  `FEEDBACKID` int(11) NOT NULL,
+  `APPLICANTID` int(11) NOT NULL,
+  `REGISTRATIONID` int(11) NOT NULL,
+  `FEEDBACK` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbljob`
+--
+
+CREATE TABLE `tbljob` (
+  `JOBID` int(11) NOT NULL,
+  `COMPANYID` int(11) NOT NULL,
+  `CATEGORY` varchar(250) NOT NULL,
+  `OCCUPATIONTITLE` varchar(90) NOT NULL,
+  `REQ_NO_EMPLOYEES` int(11) NOT NULL,
+  `SALARIES` double NOT NULL,
+  `DURATION_EMPLOYEMENT` varchar(90) NOT NULL,
+  `QUALIFICATION_WORKEXPERIENCE` text NOT NULL,
+  `JOBDESCRIPTION` text NOT NULL,
+  `PREFEREDSEX` varchar(30) NOT NULL,
+  `SECTOR_VACANCY` text NOT NULL,
+  `JOBSTATUS` varchar(90) NOT NULL,
+  `DATEPOSTED` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `tbljob`
+--
+
+INSERT INTO `tbljob` (`JOBID`, `COMPANYID`, `CATEGORY`, `OCCUPATIONTITLE`, `REQ_NO_EMPLOYEES`, `SALARIES`, `DURATION_EMPLOYEMENT`, `QUALIFICATION_WORKEXPERIENCE`, `JOBDESCRIPTION`, `PREFEREDSEX`, `SECTOR_VACANCY`, `JOBSTATUS`, `DATEPOSTED`) VALUES
+(1, 2, 'Technology', 'ISD', 6, 15000, 'jan 30', 'Two year Experience', 'We are looking for bachelor of science in information technology.\r\nasdasdasd', 'Male/Female', 'yes', '', '2018-05-20 00:00:00'),
+(2, 2, 'Technology', 'Accounting', 1, 15000, 'may 20', 'Two years Experience', 'We are looking for bachelor of science in Acountancy', 'Female', 'yes', '', '2018-05-20 02:33:00'),
+(3, 3, 'Degital Marketing', 'staff', 0, 6000000, '5', 'tes', 'tes', 'Male/Female', 'tes', '', '2023-08-16 06:52:00');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbljobregistration`
+--
+
+CREATE TABLE `tbljobregistration` (
+  `REGISTRATIONID` int(11) NOT NULL,
+  `COMPANYID` int(11) NOT NULL,
+  `JOBID` int(11) NOT NULL,
+  `APPLICANTID` int(11) NOT NULL,
+  `APPLICANT` varchar(90) NOT NULL,
+  `REGISTRATIONDATE` date NOT NULL,
+  `REMARKS` varchar(255) NOT NULL DEFAULT 'Pending',
+  `FILEID` varchar(30) DEFAULT NULL,
+  `PENDINGAPPLICATION` tinyint(1) NOT NULL DEFAULT 1,
+  `HVIEW` tinyint(1) NOT NULL DEFAULT 1,
+  `DATETIMEAPPROVED` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `tbljobregistration`
+--
+
+INSERT INTO `tbljobregistration` (`REGISTRATIONID`, `COMPANYID`, `JOBID`, `APPLICANTID`, `APPLICANT`, `REGISTRATIONDATE`, `REMARKS`, `FILEID`, `PENDINGAPPLICATION`, `HVIEW`, `DATETIMEAPPROVED`) VALUES
+(1, 2, 2, 2018013, 'Kim Domingo', '2018-05-27', 'Ive seen your work and its really interesting', '2147483647', 0, 1, '2018-05-26 16:13:01'),
+(2, 2, 2, 2018015, 'Janry Tan', '2018-05-26', 'aasd', '2147483647', 0, 0, '2018-05-28 14:14:45'),
+(3, 3, 3, 2023016, 'tes yes', '2023-08-16', 'Pending', '20236912529', 1, 1, '2023-08-16 06:55:00');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tblusers`
+--
+
+CREATE TABLE `tblusers` (
+  `USERID` varchar(30) NOT NULL,
+  `FULLNAME` varchar(40) NOT NULL,
+  `USERNAME` varchar(90) NOT NULL,
+  `PASS` varchar(90) NOT NULL,
+  `ROLE` varchar(30) NOT NULL,
+  `PICLOCATION` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `tblusers`
+--
+
+INSERT INTO `tblusers` (`USERID`, `FULLNAME`, `USERNAME`, `PASS`, `ROLE`, `PICLOCATION`) VALUES
+('00018', 'Job', 'admin', 'f865b53623b121fd34ee5426c792e5c33af8c227', 'Administrator', 'photos/Untitled.jpg'),
+('2018001', 'Chambe Narciso', 'Narciso', 'f3593fd40c55c33d1788309d4137e82f5eab0dea', 'Employee', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tblapplicants`
+--
+ALTER TABLE `tblapplicants`
+  ADD PRIMARY KEY (`APPLICANTID`);
+
+--
+-- Indeks untuk tabel `tblattachmentfile`
+--
+ALTER TABLE `tblattachmentfile`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indeks untuk tabel `tblautonumbers`
+--
+ALTER TABLE `tblautonumbers`
+  ADD PRIMARY KEY (`AUTOID`);
+
+--
+-- Indeks untuk tabel `tblcategory`
+--
+ALTER TABLE `tblcategory`
+  ADD PRIMARY KEY (`CATEGORYID`);
+
+--
+-- Indeks untuk tabel `tblcompany`
+--
+ALTER TABLE `tblcompany`
+  ADD PRIMARY KEY (`COMPANYID`);
+
+--
+-- Indeks untuk tabel `tblemployees`
+--
+ALTER TABLE `tblemployees`
+  ADD PRIMARY KEY (`INCID`),
+  ADD UNIQUE KEY `EMPLOYEEID` (`EMPLOYEEID`);
+
+--
+-- Indeks untuk tabel `tblfeedback`
+--
+ALTER TABLE `tblfeedback`
+  ADD PRIMARY KEY (`FEEDBACKID`);
+
+--
+-- Indeks untuk tabel `tbljob`
+--
+ALTER TABLE `tbljob`
+  ADD PRIMARY KEY (`JOBID`);
+
+--
+-- Indeks untuk tabel `tbljobregistration`
+--
+ALTER TABLE `tbljobregistration`
+  ADD PRIMARY KEY (`REGISTRATIONID`);
+
+--
+-- Indeks untuk tabel `tblusers`
+--
+ALTER TABLE `tblusers`
+  ADD PRIMARY KEY (`USERID`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tblapplicants`
+--
+ALTER TABLE `tblapplicants`
+  MODIFY `APPLICANTID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2023017;
+
+--
+-- AUTO_INCREMENT untuk tabel `tblattachmentfile`
+--
+ALTER TABLE `tblattachmentfile`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `tblautonumbers`
+--
+ALTER TABLE `tblautonumbers`
+  MODIFY `AUTOID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `tblcategory`
+--
+ALTER TABLE `tblcategory`
+  MODIFY `CATEGORYID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT untuk tabel `tblcompany`
+--
+ALTER TABLE `tblcompany`
+  MODIFY `COMPANYID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT untuk tabel `tblemployees`
+--
+ALTER TABLE `tblemployees`
+  MODIFY `INCID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
+
+--
+-- AUTO_INCREMENT untuk tabel `tblfeedback`
+--
+ALTER TABLE `tblfeedback`
+  MODIFY `FEEDBACKID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbljob`
+--
+ALTER TABLE `tbljob`
+  MODIFY `JOBID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbljobregistration`
+--
+ALTER TABLE `tbljobregistration`
+  MODIFY `REGISTRATIONID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
